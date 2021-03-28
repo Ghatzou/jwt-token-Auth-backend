f71e54c0bedc2e517c0834bcb97b9f7ca81e78ab
TAGHEZOUIT amine <aminetaghezouitbus@hotmail.com>
2021-03-28 06:09:42 +0200

    fix err

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 //use request validation for registration
-use App\Http\Requests;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
